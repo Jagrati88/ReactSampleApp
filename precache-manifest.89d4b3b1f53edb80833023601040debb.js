556246e6a0cc0fdc07ae6b8c7358f24b05dea84f
Jagrati Gupta <jagrati.gupta@thepsi.com>
2020-03-28 12:57:59 +0530

    Updates

---
+++ precache-manifest.89d4b3b1f53edb80833023601040debb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babf33940db18a6fcfbfef86bab04d28",
+    "url": "/ReactSampleApp/index.html"
+  },
+  {
+    "revision": "ad47fafa24dc1be4e1f4",
+    "url": "/ReactSampleApp/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "dd18b876319b37b55128",
+    "url": "/ReactSampleApp/static/js/2.8f2e10f3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactSampleApp/static/js/2.8f2e10f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad47fafa24dc1be4e1f4",
+    "url": "/ReactSampleApp/static/js/main.9176002e.chunk.js"
+  },
+  {
+    "revision": "bccf40cbef44805e7000",
+    "url": "/ReactSampleApp/static/js/runtime-main.e2112c39.js"
+  }
+]);
